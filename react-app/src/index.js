fdfc4236055a664f58c29646d1a7e46f5ef6d698
koran23 <ahdari33@gmail.com>
2021-01-25 15:37:33 -0700

    css

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import configureStore from './store';
-import { restoreCSRF, fetch } from './store/csrf';
-import * as sessionActions from './store/session';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+import { restoreCSRF, fetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
+import GlobalStyle from "./styles/GlobalStyle";
+import { Theme } from "./styles/Theme";
+import { ThemeProvider } from "styled-components";
+import { ToastContainer } from "react-toastify";
 
 const store = configureStore();
 
@@ -19,9 +23,17 @@ if (process.env.NODE_ENV !== "production") {
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App/>
-    </Provider>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <ToastContainer
+        autoClose={2500}
+        position="top-right"
+        closeButton={false}
+      />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
