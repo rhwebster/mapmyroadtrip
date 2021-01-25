a93b4430b2059571fe2cfc11b523b4032e3cbec9
Sbutler8 <samibutler1992@gmail.com>
2021-01-25 12:47:49 -0700

    Adding auth/ files andpsycopg2 dependency, fixed initial render to '/' page

---
+++ react-app/src/components/auth/LoginForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { login } from "../../services/auth";
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from "../../store/session";
+
+const LoginForm = ({ authenticated, setAuthenticated }) => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  // console.log(user)
+  const [errors, setErrors] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    setErrors([]);
+
+    dispatch(sessionActions.login({email, password}))
+
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  if (authenticated) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <form onSubmit={onLogin}>
+      <div>
+        {errors.map((error) => (
+          <div>{error}</div>
+        ))}
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={updateEmail}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={updatePassword}
+        />
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
