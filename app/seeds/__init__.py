a6fd6aa2516d2ca4aa4dad977ad9abc71823d537
fairy-scary <ecoautumn@gmail.com>
2021-01-26 08:57:02 -0600

    configured starter seed data and added public/images folder

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,5 @@
 from flask.cli import AppGroup
-from .users import seed_users, undo_users
+from .starter import seed_users, undo_users, seed_trips, undo_trips, seed_journal_entries, undo_journal_entries, seed_photos, undo_photos
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -9,10 +9,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
-    # Add other seed functions here
+    seed_trips()
+    seed_journal_entries()
+    seed_photos()
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    # Add other undo functions here
+    undo_trips()
+    undo_journal_entries()
+    undo_photos()
