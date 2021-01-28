f681717c0cdd695a5cae8d52dccd7b1109d7eef2
fairy-scary <ecoautumn@gmail.com>
2021-01-28 09:17:38 -0600

    Primitive field components and more form code

--- react-app/src/components/CreateTripForm/index.js
+++ react-app/src/components/CreateTripForm/index.js
@@ -1,62 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
-import {addTrip} from '../../store/trips'
-import { Formik, Form, useField, useFormikContext } from 'formik';
+import {addTrip} from '../../store/trip'
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import DatePicker from 'react-datepicker';
+import { TextField, DatePickerField, RadioField } from '../FormFields'
 import PlacesAutoComplete from 'react-places-autocomplete';
 import { geocodeByAddress, getLatLng, } from 'react-places-autocomplete';
 // import css
 
-const TextField = ({ label, ...props }) => {
-    const [field, meta] = useField(props);
-    return (
-        <>
-        <label htmlFor={props.id || props.name}>{label}</label>
-        <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-        </>
-    );
-};
-
-const DatePickerField = ({ label, ...props }) => {
-    const { setFieldValue } = useFormikContext();
-    const [field] = useField(props);
-    return (
-        <>
-        <label htmlFor={props.id || props.name}>{label}</label>
-        <DatePicker
-            {...field}
-            {...props}
-            selected={(field.value && new Date(field.value)) || null}
-            onChange={val => {
-                setFieldValue(field.name, val);
-            }}
-         />
-        </>
-    );
-};
-
-const PrivacyPickerField = ({ label, ...props }) => {
-    const [field] = useField(props);
-    return (
-        <>
-        <label htmlFor={props.id || props.name}>{label}</label>
-        <Field type="radio" {...field} {...props} />
-        </>
-    )
-
-}
-
-<label>
-              <Field type="radio" name="picked" value="One" />
-              One
-            </label>
-            <label>
-              <Field type="radio" name="picked" value="Two" />
-              Two
-            </label>
 
 const CreateTripForm = () => {
     const dispatch = useDispatch();
@@ -105,17 +55,28 @@ const CreateTripForm = () => {
                         label="End Date of Trip"
                         name="end_date"
                     />
-
-
-
+                    <
+                    <label>
+                    <Field 
+                        type="radio"    
+                        name="private" 
+                        value={true} 
+                    />
+                    Private
+                    </label>
+                    <label>
+                    <Field 
+                        type="radio" 
+                        name="private" 
+                        value={false} 
+                    />
+                    Public
+                    </label>
+                    <button type="submit">Create Trip</button>
                 </Form>
-
-
-
             </Formik>
         </div>
-    )
-
+    );
 };
 
 export default CreateTripForm
---
+++ react-app/src/store/entry.js
@@ -0,0 +1,31 @@
+const SET_NEW_ENTRY = "user/SET_NEW_ENTRY";
+
+export const setNewEntry = (entryData) => {
+    return {
+        type: SET_NEW_ENTRY,
+        entryData: entryData,
+    };
+};
+
+export const addEntry = (entryData) => {
+    return async (dispatch) => {
+        await fetch("****API ROUTE****", {
+            method: 'POST',
+            body: JSON.stingify(entryData),
+        });
+        dispatch(setNewEntry(entryData))
+    };
+};
+
+const initialState = [];
+
+function reducer (state = initialState, action) {
+    switch (action.type) {
+        case SET_NEW_ENTRY:
+            return action.entryData;
+        default:
+            return state;
+    };
+};
+
+export default reducer 
