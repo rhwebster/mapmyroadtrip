c116ced9e8f07dd59ca590f654bd82ea0d507afb
Sbutler8 <samibutler1992@gmail.com>
2021-01-28 10:04:25 -0700

    fix merge conflicts

--- app/helpers.py
+++ app/helpers.py
@@ -1,21 +1,17 @@
 import boto3
 import botocore
-from .config import S3_KEY, S3_SECRET, S3_BUCKET
+from .config import Config
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=S3_KEY,
-    aws_secret_access_key=S3_SECRET
+    aws_access_key_id=Config.S3_KEY,
+    aws_secret_access_key=Config.S3_SECRET
 )
-
-
 def upload_file_to_s3(file, bucket_name, acl="public-read"):
     """
     Docs: http://boto3.readthedocs.io/en/latest/guide/s3.html
     """
-
     try:
-
         s3.upload_fileobj(
             file,
             bucket_name,
@@ -25,9 +21,7 @@ def upload_file_to_s3(file, bucket_name, acl="public-read"):
                 "ContentType": file.content_type
             }
         )
-
     except Exception as e:
         print("Something Happened: ", e)
         return e
-
     return "{}{}".format(app.config["S3_LOCATION"], file.filename)
