beb378bfc4492b0acbbac74fe4e61b18a33f9b57
Sbutler8 <samibutler1992@gmail.com>
2021-01-30 22:44:47 -0700

    got journal entry in store successfully

--- react-app/src/store/entry.js
+++ react-app/src/store/entry.js
@@ -1,10 +1,26 @@
 const SET_NEW_ENTRY = "user/SET_NEW_ENTRY";
+const GET_ENTRIES = "journalEntry/GET_ENTREES";
 
 export const setNewEntry = (entryData) => {
     return {
         type: SET_NEW_ENTRY,
         entryData: entryData,
     };
+};
+
+export const getEntries = (journalEntries) => {
+    return {
+        type: GET_ENTRIES,
+        journalEntries
+    };
+};
+
+export const getAllJournalEntries = (userId) => async (dispatch) => {
+
+    const res = await fetch(`api/entry/${userId}/entries`);
+    console.log('this is the data ======>', res)
+    let data = await res.json();
+    dispatch(getEntries(data.journalEntries));
 };
 
 export const addEntry = (formObj ) => async (dispatch) => {
@@ -33,15 +49,20 @@ export const addEntry = (formObj ) => async (dispatch) => {
 //     };
 // };
 
-const initialState = {};
+const initialState = {journalEntries: []};
 
-function reducer (state = initialState, action) {
+const entryReducer = (state = initialState, action) => {
+    let newState;
     switch (action.type) {
         case SET_NEW_ENTRY:
             return {...state, [action.entryData]: action.entryData};
+        case GET_ENTRIES:
+            newState = Object.assign({}, state);
+            newState.journalEntries = action.journalEntries;
+            return newState;
         default:
             return state;
     };
 };
 
-export default reducer
+export default entryReducer;
